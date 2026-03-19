0c32add8701c0f428e2649146295ac535ebe032a
TristanODonnell <tristanodonnell100@gmail.com>
2026-03-18 22:54:33 -0700

    reshape dag

--- src/autonomous_trading_platform/scheduler/airflow/dags/market_trading_dag.py
+++ src/autonomous_trading_platform/scheduler/airflow/dags/market_trading_dag.py
@@ -5,17 +5,8 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from autonomous_trading_platform.scheduler.jobs.check_ingestion_readiness_job import (
-    check_ingestion_readiness_job,
-)
-from autonomous_trading_platform.scheduler.jobs.run_order_reconciliation_job import (
-    run_order_reconciliation_job,
-)
-from autonomous_trading_platform.scheduler.jobs.run_order_submission_job import (
-    run_order_submission_job,
-)
-from autonomous_trading_platform.scheduler.jobs.run_trading_evaluation_job import (
-    run_trading_evaluation_job,
+from autonomous_trading_platform.scheduler.jobs.run_trading_cycle import (
+    run_trading_cycle,
 )
 
 with DAG(
@@ -24,24 +15,7 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     catchup=False,
 ) as dag:
-    readiness = PythonOperator(
-        task_id="check_ingestion_readiness",
-        python_callable=check_ingestion_readiness_job,
+    run_cycle = PythonOperator(
+        task_id="run_trading_cycle",
+        python_callable=run_trading_cycle,
     )
-
-    evaluate = PythonOperator(
-        task_id="run_trading_evaluation",
-        python_callable=run_trading_evaluation_job,
-    )
-
-    submit = PythonOperator(
-        task_id="run_order_submission",
-        python_callable=run_order_submission_job,
-    )
-
-    reconcile = PythonOperator(
-        task_id="run_order_reconciliation",
-        python_callable=run_order_reconciliation_job,
-    )
-
-    readiness >> evaluate >> submit >> reconcile
